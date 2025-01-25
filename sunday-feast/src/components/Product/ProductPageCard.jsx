0b521a7665fdd3a0c34eb08898a800af896a89a8
sumanth <you@example.com>
2025-01-25 11:53:22 +0530

    Added Product Details card

--- sunday-feast/src/components/Product/ProductPageCard.jsx
+++ sunday-feast/src/components/Product/ProductPageCard.jsx
@@ -13,11 +13,12 @@ import {
 
 function ProductPageCard({productImg, title, description,price,weight,quantity, productId}) {
   return (
-    <Card className="border-2 p-3 border-black border-solid xl:w-[70em] xl:h-[30em] mb:w-[15em] mb:h-[30em] relative flex flex-row">
-        <CardHeader className="border-2 border-black border-solid h-full  m-1 w-1/2">
+    <>
+    <Card className="border-2 p-3 border-black border-solid xl:w-[70em] xl:h-[30em] mb:w-[18em] mb:h-[20em] relative flex xl:flex-row">
+        <CardHeader className="border-2 border-black border-solid xl:h-full  m-1 xl:w-1/2 mb:h-full mb:w-full">
             <Image src={productImg} alt={title} className="w-full h-full object-cover" />
         </CardHeader>
-        <CardContent className="border-2 border-black border-solid h-full m-1 w-1/2 flex flex-col justify-between">
+        <CardContent className="border-2 border-black border-solid h-full m-1 w-1/2 flex-col justify-between xl:flex mb:hidden">
             <CardTitle className="text-3xl mt-10">{title}</CardTitle>
             <CardDescription className="text-xl mb-3">{description}</CardDescription>
             <CardDescription className="flex flex-row items-center">Price:<IndianRupeeIcon className='size-3' />{price}</CardDescription>
@@ -27,7 +28,9 @@ function ProductPageCard({productImg, title, description,price,weight,quantity, 
               <Button className="self-center hover:bg-red-300 hover:scale-105 text-xl p-[1.2em]"> Add to Cart </Button>
             </CardFooter>
         </CardContent>
+        
     </Card>
+    </>
   )
 }
 
